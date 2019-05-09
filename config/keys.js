173b6ed774a08936bd7b58038aa8de7fc451b252
darshita1108 <aggarwaldarshita@gmail.com>
2019-05-09 21:56:36 +0530

    done

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,7 @@
 module.exports={
 	mongoURI:'mongodb://user123:user123@ds217125.mlab.com:17125/acms',
-<<<<<<< HEAD
 	key:'54383ddcc3734cab8ce0e83f911be831',
 	password:'daaru@1108'
-}
-=======
-	key:'AIzaSyA1KGt0fNR5NDB_Jl4mdKmkb1Imf5Jzn4c',
 	
 }
->>>>>>> 865f0922580de447f2af08b712057668dbfe8a22
+
